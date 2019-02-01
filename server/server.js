2c62c6c56a8fcb367da280926cce7eb4a1b58e6c
Victor Zhou <vzhou842@gmail.com>
2019-02-01 14:46:54 -0400

    Initial setup for GET /link/:id requests

--- server/server.js
+++ server/server.js
@@ -14,14 +14,26 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(BUILD_DIR, 'index.html'));
 });
 
+app.get('/link/:id', (req, res) => {
+  const { id } = req.params;
+  // TODO: get csv from link id
+  if (true) {
+    // Link not found
+    res.sendFile(path.join(BUILD_DIR, 'link404.html'));
+  } else {
+
+  }
+});
+
 app.post('/link/new', (req, res) => {
   const { csv } = req.body;
   if (!csv) {
     return res.status(400).end();
   }
+  // TODO: enforce max / min csv size
   // TODO: save csv with link
   const url = shortid();
   res.status(200).send(`/link/${url}`);
 });
 
-var server = app.listen(3000 || process.env.PORT);
+app.listen(3000 || process.env.PORT);
